d96e821402593f0adc11203502e16d46af8a98db
teru <incredibleteru@gmail.com>
2023-01-10 22:51:52 +0900

    setup react config & enable data fetch from server to react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
 
-function App() {
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+  const [text, setText] = React.useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api')
+      const data = await response.text()
+      // responseとしてjsonを返す場合は、dataの定義は以下
+      // const data = await response.json()
+      setText(data)
+    }
+    fetchData().catch(console.error)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {text ? <p>{text} from our flask api 😀</p> : <p> Loading ... maybe server is not running</p>}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
