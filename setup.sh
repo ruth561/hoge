d34e082e02ed4b3c69a66abfabbfd9874aa1848e
takumi jin <primenumber_2_3_5@yahoo.co.jp>
2023-01-11 13:29:40 +0900

    NASへの接続ができるようになった。
    ./setup.shのコメントアウトを修正しておきました。

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 export FLASK_APP=backend/main.py
 export FLASK_ENV=development
 
-// ampersand before flask run backgrounds the process
-// i.e., 'yarn start' is fired after 'flask run'
+# ampersand before flask run backgrounds the process
+# i.e., 'yarn start' is fired after 'flask run'
 (cd frontend 
 yarn start) & 
 flask run
