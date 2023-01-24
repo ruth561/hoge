9770417ffedadc3986e8399646b86dab948846a2
teru <incredibleteru@gmail.com>
2023-01-24 23:55:49 +0900

    add pdf upload button in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Document, Page, pdfjs } from 'react-pdf'
+// eslint-disable-next-line import/no-unresolved
+import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import { useForm } from 'react-hook-form'
 
 type Seminar = {
   id: string
@@ -13,9 +19,15 @@ type SeminarData = {
   '02': Seminar
 }
 
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+  standardFontDataUrl: 'standard_fonts/',
+}
+
 const App = () => {
-  const [seminars, setSeminars] = React.useState<Seminar[]>([])
-  const [allSeminarIds, setAllSeminarIds] = React.useState<string[]>([])
+  const [seminars, setSeminars] = useState<Seminar[]>([])
+  const [allSeminarIds, setAllSeminarIds] = useState<string[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,37 +43,110 @@ const App = () => {
     fetchData().catch(console.error)
   }, [])
 
+  const [currentDocument, setCurrentDocument] = useState<{ seminar: string; document: string } | null>(null)
+  const [currentDocumentURL, setcurrentDocumentURL] = useState<string | null>(null)
+
   const handleOnClickDocument = (seminar: Seminar, document: string) => {
-    const fetchDocumentData = async () => {
-      const response = await fetch(`/nas/${seminar.id}/${document}`)
-      const data = await response.text()
-      if (data !== currentDocument) setCurrentDocument(data)
-    }
-    fetchDocumentData().catch(console.error)
+    if (currentDocument?.seminar === seminar.id && currentDocument?.document === document) return
+    setCurrentDocument({ seminar: seminar.id, document })
   }
 
-  const [currentDocument, setCurrentDocument] = React.useState<string | null>(null)
+  useEffect(() => {
+    if (!currentDocument) return
+    const fetchDocumentData = async () => {
+      const url = `/nas/${currentDocument.seminar}/${currentDocument.document}`
+      const response = await fetch(url)
+      const blob = await response.blob()
+      const blobURL = URL.createObjectURL(blob)
+      setcurrentDocumentURL(blobURL)
+    }
+    fetchDocumentData().catch(console.error)
+  }, [currentDocument])
+
+  const [numPages, setNumPages] = useState<number | null>(null)
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+  }, [])
+
+  function onDocumentLoadSuccess(pdf: PDFDocumentProxy) {
+    setNumPages(pdf.numPages)
+  }
+
+  /*
+   * 意図としては、seminarId に 新しい document (pdf) を追加するために
+   * POSTリクエストを送っている。
+   * 注意として、POSTリクエストのbodyに入っているデータ自体と、そのデータの解釈（サーバー上でどう扱うか。craeteするかupdateするか）
+   * についてはfrontとbackで合意が必要。
+   * */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = async (data: any) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    }
+    console.log(data)
+    const response = await fetch('/api/seminars/update', requestOptions)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const jsonData = await response.json()
+    console.log(jsonData)
+  }
 
   return (
     <div>
-      {seminars.map((seminar) => (
-        <div key={seminar.id}>
-          <h1>{seminar.title}</h1>
-          <h2>{seminar.author}</h2>
-          <h3>{seminar.date}</h3>
-          <ul>
-            {seminar.documents.map((document) => (
-              <li>
-                {document}
-                <button type="button" onClick={() => handleOnClickDocument(seminar, document)}>
-                  open
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-      {currentDocument ? <div>{currentDocument}</div> : null}
+      <div style={{ display: 'flex', gap: '100px' }}>
+        {seminars.map((seminar) => (
+          <div key={seminar.id}>
+            <h1>{seminar.title}</h1>
+            <h2>{seminar.author}</h2>
+            <h3>{seminar.date}</h3>
+            <ul>
+              {seminar.documents.map((document) => (
+                <li>
+                  {document}
+                  <button type="button" onClick={() => handleOnClickDocument(seminar, document)}>
+                    open
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <hr className="solid" />
+      <h1>プレビュー</h1>
+      {currentDocumentURL ? (
+        <Document
+          file={{ url: currentDocumentURL }}
+          onLoadSuccess={(pdf) => onDocumentLoadSuccess(pdf)}
+          options={options}
+          renderMode="svg"
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      ) : null}
+      <hr className="solid" />
+      <h1>アップロード</h1>
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+      <form action="/api/upload" method="post" encType="multipart/form-data" onSubmit={handleSubmit(onSubmit)}>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <select {...register('seminar-id')}>
+          {allSeminarIds.map((id) => (
+            <option id="seminar id">{id}</option>
+          ))}
+        </select>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <input type="file" {...register('file')} />
+        <button type="submit">アップロード</button>
+      </form>
     </div>
   )
 }
