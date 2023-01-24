ebf8ccb6cad73393b286dcbcdcaf5dd1f480e8a2
teru <incredibleteru@gmail.com>
2023-01-24 21:03:02 +0900

    enable GET seminar info

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,67 @@
 import React, { useEffect } from 'react'
 
-import logo from './logo.svg'
-import './App.css'
+type Seminar = {
+  id: string
+  title: string
+  author: string
+  date: string
+  documents: string[]
+}
+
+type SeminarData = {
+  '01': Seminar
+  '02': Seminar
+}
 
 const App = () => {
-  const [text, setText] = React.useState('')
+  const [seminars, setSeminars] = React.useState<Seminar[]>([])
+  const [allSeminarIds, setAllSeminarIds] = React.useState<string[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api')
+      const response = await fetch('/api/all')
       const data = await response.text()
-      // responseとしてjsonを返す場合は、dataの定義は以下
-      // const data = await response.json()
-      setText(data)
+      const seminarData = JSON.parse(data) as SeminarData
+      setSeminars([
+        { ...seminarData['01'], id: '01' },
+        { ...seminarData['02'], id: '02' },
+      ])
+      setAllSeminarIds(['01', '02'])
     }
     fetchData().catch(console.error)
   }, [])
 
+  const handleOnClickDocument = (seminar: Seminar, document: string) => {
+    const fetchDocumentData = async () => {
+      const response = await fetch(`/nas/${seminar.id}/${document}`)
+      const data = await response.text()
+      if (data !== currentDocument) setCurrentDocument(data)
+    }
+    fetchDocumentData().catch(console.error)
+  }
+
+  const [currentDocument, setCurrentDocument] = React.useState<string | null>(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {text ? <p>{text} from our flask api 😀</p> : <p> Loading ... maybe server is not running</p>}
-      </header>
+    <div>
+      {seminars.map((seminar) => (
+        <div key={seminar.id}>
+          <h1>{seminar.title}</h1>
+          <h2>{seminar.author}</h2>
+          <h3>{seminar.date}</h3>
+          <ul>
+            {seminar.documents.map((document) => (
+              <li>
+                {document}
+                <button type="button" onClick={() => handleOnClickDocument(seminar, document)}>
+                  open
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      {currentDocument ? <div>{currentDocument}</div> : null}
     </div>
   )
 }
